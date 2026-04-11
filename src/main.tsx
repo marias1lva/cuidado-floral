4a2f7fa6ad0e2585ac85c8c819a62892d955d5c2
Nicoly Colutti <nicolymaria.tejerocolutti@hotmail.com>
2026-04-11 19:44:18 -0300

    feat: implementado telas de login e cadastro

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,21 @@
-import React from "react";
+import React, { useState } from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
+import { AuthScreen } from "./authScreen";
 import { VolunteerArea } from "./volunteerArea";
 
-ReactDOM.createRoot(document.getElementById("root")!).render(
-  <React.StrictMode>
-    <VolunteerArea />
-  </React.StrictMode>,
-);
+function App() {
+  const [isLogged, setIsLogged] = useState(false);
+
+  return (
+    <React.StrictMode>
+      {isLogged ? (
+        <VolunteerArea />
+      ) : (
+        <AuthScreen onLoginSuccess={() => setIsLogged(true)} />
+      )}
+    </React.StrictMode>
+  );
+}
+
+ReactDOM.createRoot(document.getElementById("root")!).render(<App />);
