444e3b80210e950a820626177abd02465bfdafc0
marias1lva <mariaesilva2308@outlook.com>
2026-04-13 16:59:16 -0300

    feat: implement main App component to manage user roles and navigation

--- src/main.tsx
+++ src/main.tsx
@@ -1,21 +1,6 @@
-import React, { useState } from "react";
 import ReactDOM from "react-dom/client";
+
+import { App } from "./App";
 import "./index.css";
-import { AuthScreen } from "./authScreen";
-import { VolunteerArea } from "./volunteerArea";
-
-function App() {
-  const [isLogged, setIsLogged] = useState(false);
-
-  return (
-    <React.StrictMode>
-      {isLogged ? (
-        <VolunteerArea />
-      ) : (
-        <AuthScreen onLoginSuccess={() => setIsLogged(true)} />
-      )}
-    </React.StrictMode>
-  );
-}
 
 ReactDOM.createRoot(document.getElementById("root")!).render(<App />);
